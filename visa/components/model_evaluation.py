7f974ad34eaa26714b5f83ec8a73eb82ced654dd
Mukul <mukulsingh078968@gmail.com>
2023-05-13 09:28:44 +0530

    updated

--- visa/components/model_evaluation.py
+++ visa/components/model_evaluation.py
@@ -1,61 +1,62 @@
 from visa.logger import logging
-import sys , os
+import sys
 import numpy as np
 from visa.exception import CustomException
 from visa.entity.config_entity import ModelEvaluationConfig
 from visa.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact,ModelTrainerArtifact,ModelEvaluationArtifact
 from visa.constant import *
-from visa.utils.utils import write_yaml_file,read_yaml_file,load_object,load_data
+from visa.utils.utils import write_yaml_file, read_yaml_file, load_object, load_data
 from visa.entity.model_factory import evaluate_classification_model
 
-# get the best model
-# updated our model everytime
-# Inititate our model 
+
+# Get the best model
+# Updated our model everytime
+# Inititate our  model
 
 class ModelEvaluation:
 
-    def __init__(self,model_evaluation_config: ModelEvaluationConfig,
+    def __init__(self, model_evaluation_config: ModelEvaluationConfig,
                  data_ingestion_artifact: DataIngestionArtifact,
                  data_validation_artifact: DataValidationArtifact,
                  model_trainer_artifact: ModelTrainerArtifact):
-        
         try:
-            logging.info(f"{'>>'*20} Model Evaluation {'<<' * 20}")
+            logging.info(f"{'>>' * 30}Model Evaluation log started.{'<<' * 30} ")
             self.model_evaluation_config = model_evaluation_config
             self.model_trainer_artifact = model_trainer_artifact
             self.data_ingestion_artifact = data_ingestion_artifact
             self.data_validation_artifact = data_validation_artifact
         except Exception as e:
-            raise CustomException(e,sys) from e
-        
+            raise CustomException(e, sys) from e
+
     def get_best_model(self):
         try:
             model = None
             model_evaluation_file_path = self.model_evaluation_config.model_evaluation_file_path
 
             if not os.path.exists(model_evaluation_file_path):
-                write_yaml_file(file_path = model_evaluation_file_path)
+                write_yaml_file(file_path=model_evaluation_file_path,
+                                )
                 return model
-            
             model_eval_file_content = read_yaml_file(file_path=model_evaluation_file_path)
 
             model_eval_file_content = dict() if model_eval_file_content is None else model_eval_file_content
 
             if BEST_MODEL_KEY not in model_eval_file_content:
                 return model
-            
-            model = load_object(file_path = model_eval_file_content[BEST_MODEL_KEY][MODEL_PATH_KEY])
+
+            model = load_object(file_path=model_eval_file_content[BEST_MODEL_KEY][MODEL_PATH_KEY])
             return model
         except Exception as e:
-            raise CustomException(e,sys) from e
-        
-    def updated_evaluation_report(self,model_evaluation_artifact: ModelEvaluationArtifact):
+            raise CustomException(e, sys) from e
+
+    def update_evaluation_report(self, model_evaluation_artifact: ModelEvaluationArtifact):
         try:
             eval_file_path = self.model_evaluation_config.model_evaluation_file_path
-            model_eval_content = read_yaml_file(file_path = eval_file_path)
+            model_eval_content = read_yaml_file(file_path=eval_file_path)
             model_eval_content = dict() if model_eval_content is None else model_eval_content
-
-            previous_best_model =None
+            
+            
+            previous_best_model = None
             if BEST_MODEL_KEY in model_eval_content:
                 previous_best_model = model_eval_content[BEST_MODEL_KEY]
 
@@ -156,5 +157,3 @@ class ModelEvaluation:
 
     def __del__(self):
         logging.info(f"{'=' * 20}Model Evaluation log completed.{'=' * 20} ")
-        
-        
