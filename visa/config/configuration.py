c3a3fee0ff66d0520f99859c62e20a53dc4b2f36
Mukul <mukulsingh078968@gmail.com>
2023-05-18 15:12:50 +0530

    updated

--- visa/config/configuration.py
+++ visa/config/configuration.py
@@ -5,6 +5,7 @@ from visa.exception import CustomException
 from visa.entity.config_entity import *
 from visa.utils.utils import read_yaml_file
 
+
 class Configuration:
 
     def __init__(self,
@@ -179,6 +180,19 @@ class Configuration:
             
             logging.info(f"Model Evaluation Config : {response}")
             return response
+        except Exception as e:
+            raise CustomException(e,sys) from e
+        
+    def get_model_pusher_config(self) ->ModelPusherConfig:
+
+        try:
+            
+            time_stamp = f"{datetime.now().strftime('%Y%m%d%H%M%S')}"
+            model_pusher_config_info = self.config_info[MODEL_PUSHER_CONFIG_KEY]
+            export_dir_path = os.path.join(ROOT_DIR,model_pusher_config_info[MODEL_PUSHER_MODEL_EXPORT_DIR_KEY],time_stamp)
+            model_pusher_config = ModelPusherConfig(export_dir_path=export_dir_path)
+            logging.info(f"Model Pusher config {model_pusher_config}")
+            return model_pusher_config
         except Exception as e:
             raise CustomException(e,sys) from e
         
